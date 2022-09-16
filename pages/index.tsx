fe07f41f3e8786302e2ef78dd239975af5e918f9
Steve Murch <ceo@bigoven.com>
2022-09-16 13:27:42 -0700

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { FaExchangeAlt, FaLeaf } from "react-icons/fa";
 
 import Button from "../components/button";
 import Head from "next/head";
-import Image from "next/image";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
@@ -44,19 +43,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
