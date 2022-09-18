a20998083fd782a4f3ee2fa6d04b748fb746204f
Steve Murch <steve@murch.net>
2022-09-18 12:05:21 -0700

    update to flask docs

--- raspberrypi/python/rose.py
+++ raspberrypi/python/rose.py
@@ -1,11 +1,11 @@
 # Raspberry Pi app -- install this on your RPi. 
 # Uses GPIO pins to drive solenoids and potentially LED lighting
 
-# To run the Flask app, you need to set an environment variable
-
 # ! TO RUN THE FLASK APP, YOU NEED TO DO THIS
 # export FLASK_APP=rose.py
 # flask run --host=0.0.0.0 --port=5001
+# OR
+# python3 -m flask run --host=0.0.0.0 --port=5001
 
 # set that as a startup app 
 
