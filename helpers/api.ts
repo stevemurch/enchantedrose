d802063570afafca1fa6e78e7510e2769b04cde4
Steve Murch <ceo@bigoven.com>
2022-12-27 10:48:10 -0800

    change to drop code

--- helpers/api.ts
+++ helpers/api.ts
@@ -2,7 +2,7 @@ import { ROSE_BASE_URL } from "../globals";
 import axios from "axios";
 
 export const dropPetal = (num: number): Promise<string> => {
-  const url = `${ROSE_BASE_URL}/activate/${num}`;
+  const url = `${ROSE_BASE_URL}/drop/${num}`;
 
   return new Promise<string>((resolve, reject) => {
     axios
